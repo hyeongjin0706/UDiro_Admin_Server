ae0f2b1ef2ab1422c3762d3ccf863e99717ae980
hyeongjin0706 <92289971+hyeongjin0706@users.noreply.github.com>
2023-06-07 09:27:26 +0900

    Update index.js

--- index.js
+++ index.js
@@ -9,9 +9,13 @@ import {config} from "./config.js";
 import {sequelize} from "./db/database.js"
 
 const app = express();
+const corsOption = {
+    origin: config.cors.allowedOrigin,
+    optionsSuccessStatus: 200
+};
 
 app.use(express.json());
-app.use(cors());
+app.use(cors(corsOption));
 app.use(morgan("tiny"));
 
 app.use("/auth", authRouter);
