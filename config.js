5ccc4b95126b0ebd047a61ec27d55ae89d453976
hyeongjin0706 <92289971+hyeongjin0706@users.noreply.github.com>
2023-06-07 09:28:42 +0900

    Update config.js

--- config.js
+++ config.js
@@ -25,7 +25,7 @@ export const config = {
         user:required("DB_USER"),
         database:required("DB_DATABASE"),
         password:required("DB_PASSWORD")
-    }
+    },
     cors: {
         allowedOrigin: required('CORS_ALLOW_ORIGIN')
     }
